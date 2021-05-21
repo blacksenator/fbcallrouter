0fe6f215cc1a00b9af6a3048f6f5821db469371f
Black Senator <knuffy@anasco.de>
2021-05-21 18:12:06 +0200

    forgotten to add outsourced file fixes #7

---
+++ assets/celluar.php
@@ -0,0 +1,54 @@
+<?php
+
+//namespace blacksenator\callrouter;
+
+/**
+ * current in use mobile phone number prefixes
+ *
+ * @see: BNetzA at https://tinyurl.com/y7648pc9
+ *
+ * There isn´t a downloadable file with this assignments.
+ * It is advisable to consult the above address from time
+ * to time and to transfer any changes to the following list:
+ */
+
+$celluarNumbers = [
+    '15019' => 'Tismi BV',
+    '1511'  => 'Telekom',
+    '1512'  => 'Telekom',
+    '1514'  => 'Telekom',
+    '1515'  => 'Telekom',
+    '1516'  => 'Telekom',
+    '1517'  => 'Telekom',
+    '1520'  => 'Vodafone',
+    '1521'  => 'Vodafone/Lyca',
+    '1522'  => 'Vodafone',
+    '1523'  => 'Vodafone',
+    '1525'  => 'Vodafone',
+    '1526'  => 'Vodafone',
+    '1529'  => 'Vodafone/Truphone',
+    '15566' => 'Drillisch',
+    '15630' => 'multiConnect',
+    '15678' => 'Argon',
+    '1570'  => 'Telefónica',
+    '1573'  => 'Telefónica',
+    '1575'  => 'Telefónica',
+    '1577'  => 'Telefónica',
+    '1578'  => 'Telefónica',
+    '1579'  => 'Telefónica/SipGate',
+    '15888' => 'TelcoVillage',
+    '1590'  => 'Telefónica',
+    '160'   => 'Telekom',
+    '162'   => 'Vodafone',
+    '163'   => 'Telefónica',
+    '170'   => 'Telekom',
+    '171'   => 'Telekom',
+    '172'   => 'Vodafone',
+    '173'   => 'Vodafone',
+    '174'   => 'Vodafone',
+    '175'   => 'Telekom',
+    '176'   => 'Telefónica',
+    '177'   => 'Telefónica',
+    '178'   => 'Telefónica',
+    '179'   => 'Telefónica',
+];
